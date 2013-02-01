cb6f13718ddd17dcd4cadbe109107709b1a42ada
Max Kolysh <maxko87@gmail.com>
2013-02-01 06:16:13 -0500

    initial commit, voting works

---
+++ bin/unit2.py
@@ -0,0 +1,7 @@
+#!/Users/mkolysh/code/text_to_vote/bin/python
+
+__unittest = True
+
+from unittest2.main import main_
+
+main_()
---
+++ index.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, redirect, render_template
+import twilio.twiml
+#from flaskext.mysql import MySQL
+import MySQLdb 
+import MySQLdb.cursors
+ 
+app = Flask(__name__)
+app.config.from_object('config')
+
+myDb = MySQLdb.connect(host=app.config['MYSQL_DATABASE_HOST'], user=app.config['MYSQL_DATABASE_USER'], passwd=app.config['MYSQL_DATABASE_PASSWORD'], db=app.config['MYSQL_DATABASE_DB'], cursorclass=MySQLdb.cursors.DictCursor)
+
+#mysql = MySQL()
+#mysql.init_app(app)
+
+@app.route("/", methods=['GET'])
+def index():
+    cursor = myDb.cursor()
+    cursor.execute('select * from teams13')
+    for team in cursor:
+        print team
+        print team['vote_id'], team['team_name'], team['votes']
+    
+    return render_template('index.html', teams=cursor)
+
+@app.route("/vote", methods=['GET', 'POST'])
+def vote():
+    cursor = myDb.cursor()
+
+    #prevent from voting twice
+    from_number = request.values.get('From', None)
+    cursor.execute('select * from phone_numbers13 where phone_number=' + from_number)
+    number = cursor.fetchone()
+    if number:
+        return "You already voted." #don't waste a text here
+
+    #cast vote
+    vote_id = request.values.get('Body', None)
+
+    cursor.execute('select * from teams13 where vote_id=' + vote_id)
+    team = cursor.fetchone()
+
+    if team:
+        message = "Your vote for " + team['team_name'] + " has been tallied!"
+        cursor.execute( 'update teams13 set votes=' + str((team['votes']+1)) + ' where vote_id=' + str(team['vote_id']) )
+        query = 'insert into phone_numbers13 values(' + "\'" + from_number + "\'" + ')' 
+        print query
+        cursor.execute(query)
+    else:
+        message = "Not a valid vote."
+ 
+    resp = twilio.twiml.Response()
+    resp.sms(message)
+
+    return message
+
+if __name__ == "__main__":
+    app.run(debug=True)
